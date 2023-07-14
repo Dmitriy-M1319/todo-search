6c3ad68bd534389ca824628ecc8ca7381ca45b2b
Dmitriy Mamonov <dimon.5642014@yandex.ru>
2023-07-15 01:48:04 +0300

    add file searching

--- main.py
+++ main.py
@@ -13,8 +13,21 @@ class ToDoLine:
 all_todos: list[ToDoLine] = []
 
 
-def find_todos(dirpath: str):
-    pass
+def find_code_files(dirpath: str, file_type: str):
+    with os.scandir(dirpath) as dir_iterator:
+        for entry in dir_iterator:
+            if entry.is_file():
+                if entry.name.endswith(file_type):
+                    find_todos(entry.path)
+            elif entry.is_dir():
+                find_code_files(entry.path, file_type)
+            else:
+                return -1
+
+
+def find_todos(filepath: str):
+    print(f'File with code: {filepath}')
+    
 
 
 def add_new_todo(line: int, text: str, file: str):
@@ -27,11 +40,16 @@ def print_todo(todo_item: ToDoLine):
 
 arg_parser = argparse.ArgumentParser(description="Find all TODO notes in directory with code")
 arg_parser.add_argument('dirpath', type=str, help='A directory with code (project)')
-arg_parser.add_argument('file-type', type=str, help='A type of file (.py, .c, .cpp and etc.)')
+arg_parser.add_argument('ftype', type=str, help='A type of file (.py, .c, .cpp and etc.)')
 
 
 def main():
-    arg_parser.parse_args()
+    args = arg_parser.parse_args()
+    if not os.path.exists(args.dirpath):
+        print(f'{args.dirpath}: No such directory')
+        return -1
+    find_code_files(args.dirpath, args.ftype)
+
 
 if __name__ == '__main__':
     main()
